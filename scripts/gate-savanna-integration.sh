dfc135c3391323099c920a7b9debe4b671ff230a
Yaroslav Lobankov <ylobankov@mirantis.com>
2013-09-04 21:32:08 +0400

    Added changes in gate-savanna-integration.sh for new integration tests

--- scripts/gate-savanna-integration.sh
+++ scripts/gate-savanna-integration.sh
@@ -43,33 +43,24 @@ export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
 
 echo "OS_USERNAME = 'ci-user'
 OS_PASSWORD = 'swordfish'
+OS_TENANT_ID = 'cc39ab50891f4eb0aec22e8d9ed553cd'
 OS_TENANT_NAME = 'ci'
 OS_AUTH_URL = 'http://172.18.168.5:35357/v2.0/'
 SAVANNA_HOST = '$ADDR'
 SAVANNA_PORT = '8386'
 FLAVOR_ID = '42'
-TIMEOUT = 25
+CLUSTER_CREATION_TIMEOUT = 15
+TELNET_TIMEOUT = 5
+ACTIVE_WORKER_TIMEOUT_FOR_NAMENODE = 5
 CLUSTER_NAME = 'ci-$BUILD_NUMBER-$GERRIT_PATCHSET_NUMBER'
 USER_KEYPAIR_ID = 'public-jenkins'
-PATH_TO_SSH = '/home/ubuntu/.ssh/id_rsa'
+PATH_TO_SSH_KEY = '/home/ubuntu/.ssh/id_rsa'
 JT_PORT = 50030
 NN_PORT = 50070
 TT_PORT = 50060
 DN_PORT = 50075
 SEC_NN_PORT = 50090
-ENABLE_CLUSTER_CL_TEMPLATE_CRUD_TESTS = True
-ENABLE_CLUSTER_NGT_NODE_PROCESS_CRUD_TESTS = True
-ENABLE_CLUSTER_NGT_CRUD_TESTS = True
-ENABLE_CLUSTER_NODE_PROCESS_CRUD_TESTS = True
-ENABLE_CL_TEMPLATE_CRUD_TESTS = True
-ENABLE_NGT_CRUD_TESTS = True
-ENABLE_HADOOP_TESTS_FOR_VANILLA_PLUGIN = True
-ENABLE_HADOOP_TESTS_FOR_HDP_PLUGIN = False
-ENABLE_SWIFT_TESTS = True
-ENABLE_SCALING_TESTS = True
-ENABLE_CONFIG_TESTS = True
-ENABLE_IR_TESTS = True
-" >> $WORKSPACE/savanna/tests/integration/configs/common_config.py
+" >> $WORKSPACE/savanna/tests/integration_new/configs/common_config.py
 
 echo "PLUGIN_NAME = 'hdp'
 IMAGE_ID = '5ea141c3-893e-4b5c-b138-910adc09b281'
@@ -78,7 +69,7 @@ HADOOP_VERSION = '1.3.0'
 HADOOP_USER = 'hdfs'
 HADOOP_DIRECTORY = '/usr/lib/hadoop'
 HADOOP_LOG_DIRECTORY = '/hadoop/mapred/userlogs'
-" >> $WORKSPACE/savanna/tests/integration/configs/hdp_config.py
+" >> $WORKSPACE/savanna/tests/integration_new/configs/hdp_config.py
 
 echo "PLUGIN_NAME = 'vanilla'
 IMAGE_ID = '75615550-013c-426f-a6e5-dd3318950c20'
@@ -87,7 +78,10 @@ HADOOP_VERSION = '1.1.2'
 HADOOP_USER = 'hadoop'
 HADOOP_DIRECTORY = '/usr/share/hadoop'
 HADOOP_LOG_DIRECTORY = '/mnt/log/hadoop/hadoop/userlogs'
-" >> $WORKSPACE/savanna/tests/integration/configs/vanilla_config.py
+SKIP_CLUSTER_CONFIG_TEST = False
+SKIP_MAP_REDUCE_TEST = False
+SKIP_SWIFT_TEST = False
+" >> $WORKSPACE/savanna/tests/integration_new/configs/vanilla_config.py
 
 i=0
 
@@ -106,7 +100,7 @@ done
 
 if [ "$FAILURE" = 0 ]; then
    
-    cd $WORKSPACE && tox -e integration
+    cd $WORKSPACE && tox -e integration -- -a tags=vanilla
 fi
 
 echo "-----------Python env-----------"
