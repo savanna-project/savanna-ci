a97a4a94e7104c2ca1a43939b668095c841b975e
Yaroslav Lobankov <ylobankov@mirantis.com>
2013-09-19 19:06:42 +0400

    Migration to Oslo config

--- scripts/gate-savanna-integration-new.sh
+++ scripts/gate-savanna-integration-new.sh
@@ -41,48 +41,50 @@ screen -dmS savanna-api /bin/bash -c "tox -evenv -- savanna-api --config-file et
 
 export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
 
-echo "OS_USERNAME = 'ci-user'
+echo "[COMMON]
+OS_USERNAME = 'ci-user'
 OS_PASSWORD = 'swordfish'
-OS_TENANT_ID = 'cc39ab50891f4eb0aec22e8d9ed553cd'
 OS_TENANT_NAME = 'ci'
 OS_AUTH_URL = 'http://172.18.168.5:35357/v2.0/'
 SAVANNA_HOST = '$ADDR'
 SAVANNA_PORT = '8386'
+SAVANNA_API_VERSION = 'v1.1'
 FLAVOR_ID = '42'
-CLUSTER_CREATION_TIMEOUT = 15
+CLUSTER_CREATION_TIMEOUT = 20
 TELNET_TIMEOUT = 5
 HDFS_INITIALIZATION_TIMEOUT = 5
 CLUSTER_NAME = 'ci-$BUILD_NUMBER-$GERRIT_PATCHSET_NUMBER'
 USER_KEYPAIR_ID = 'public-jenkins'
 PATH_TO_SSH_KEY = '/home/ubuntu/.ssh/id_rsa'
-JT_PORT = 50030
-NN_PORT = 50070
-TT_PORT = 50060
-DN_PORT = 50075
-SEC_NN_PORT = 50090
-" >> $WORKSPACE/savanna/tests/integration_new/configs/common_config.py
-
-echo "PLUGIN_NAME = 'hdp'
-IMAGE_ID = '5ea141c3-893e-4b5c-b138-910adc09b281'
-NODE_USERNAME = 'cloud-user'
-HADOOP_VERSION = '1.3.0'
-HADOOP_USER = 'hdfs'
-HADOOP_DIRECTORY = '/usr/lib/hadoop'
-HADOOP_LOG_DIRECTORY = '/hadoop/mapred/userlogs'
-" >> $WORKSPACE/savanna/tests/integration_new/configs/hdp_config.py
-
-echo "PLUGIN_NAME = 'vanilla'
-IMAGE_ID = '75615550-013c-426f-a6e5-dd3318950c20'
+[VANILLA]
+PLUGIN_NAME = 'vanilla'
+IMAGE_ID = 'e9691262-e286-46f7-aea5-9f40461b5eea'
 NODE_USERNAME = 'ubuntu'
-HADOOP_VERSION = '1.1.2'
+HADOOP_VERSION = '1.2.1'
 HADOOP_USER = 'hadoop'
 HADOOP_DIRECTORY = '/usr/share/hadoop'
 HADOOP_LOG_DIRECTORY = '/mnt/log/hadoop/hadoop/userlogs'
+HADOOP_PROCESSES_WITH_PORTS = jobtracker: 50030, namenode: 50070, tasktracker: 50060, datanode: 50075, secondarynamenode: 50090
+PROCESS_NAMES = nn: namenode, tt: tasktracker, dn: datanode
+SKIP_ALL_TESTS_FOR_PLUGIN = False
 SKIP_CLUSTER_CONFIG_TEST = False
 SKIP_MAP_REDUCE_TEST = False
 SKIP_SWIFT_TEST = False
 SKIP_SCALING_TEST = False
-" >> $WORKSPACE/savanna/tests/integration_new/configs/vanilla_config.py
+[HDP]
+PLUGIN_NAME = 'hdp'
+IMAGE_ID = 'cd63f719-006e-4541-a523-1fed7b91fa8c'
+NODE_USERNAME = 'root'
+HADOOP_VERSION = '1.3.0'
+HADOOP_USER = 'hdfs'
+HADOOP_DIRECTORY = '/usr/lib/hadoop'
+HADOOP_LOG_DIRECTORY = '/hadoop/mapred/userlogs'
+HADOOP_PROCESSES_WITH_PORTS = JOBTRACKER: 50030, NAMENODE: 50070, TASKTRACKER: 50060, DATANODE: 50075, SECONDARY_NAMENODE: 50090
+PROCESS_NAMES = nn: NAMENODE, tt: TASKTRACKER, dn: DATANODE
+SKIP_ALL_TESTS_FOR_PLUGIN = True
+SKIP_MAP_REDUCE_TEST = False
+SKIP_SCALING_TEST = False
+" >> $WORKSPACE/savanna/tests/integration_new/configs/full-config.conf
 
 i=0
 
@@ -101,7 +103,7 @@ done
 
 if [ "$FAILURE" = 0 ]; then
    
-    cd $WORKSPACE && tox -e integration -- -a tags=vanilla
+    cd $WORKSPACE && tox -e integration
 fi
 
 echo "-----------Python env-----------"
