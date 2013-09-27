61bccff7814657edff2180faa7e86fb1b68dcc63
Sergey Kolekonov <skolekonov@mirantis.com>
2013-09-27 14:12:13 +0400

    ci were moved to the new integration tests

--- scripts/gate-savanna-integration-old.sh
+++ scripts/gate-savanna-integration-old.sh
@@ -2,9 +2,8 @@
 
 cd $WORKSPACE
 
-if [ ! -f $WORKSPACE/savanna/tests/integration_new/configs/itest.conf.sample ]; then
-     exit 0
-fi
+TOX_LOG=$WORKSPACE/.tox/venv/log/venv-1.log
+TMP_LOG=/tmp/tox.log
 
 screen -S savanna-api -X quit
 rm -f /tmp/savanna-server.db
@@ -45,57 +44,37 @@ screen -dmS savanna-api /bin/bash -c "tox -evenv -- savanna-api --config-file et
 
 export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
 
-echo "[COMMON]
-OS_USERNAME = 'ci-user'
+echo "OS_USERNAME = 'ci-user'
 OS_PASSWORD = 'swordfish'
 OS_TENANT_NAME = 'ci'
 OS_AUTH_URL = 'http://172.18.168.5:35357/v2.0/'
 SAVANNA_HOST = '$ADDR'
-SAVANNA_PORT = '8386'
-SAVANNA_API_VERSION = 'v1.1'
-FLAVOR_ID = '42'
-CLUSTER_CREATION_TIMEOUT = 45
-TELNET_TIMEOUT = 5
-HDFS_INITIALIZATION_TIMEOUT = 5
 CLUSTER_NAME = 'ci-$BUILD_NUMBER-$GERRIT_PATCHSET_NUMBER'
 USER_KEYPAIR_ID = 'public-jenkins'
-PATH_TO_SSH_KEY = '/home/ubuntu/.ssh/id_rsa'
-[VANILLA]
-PLUGIN_NAME = 'vanilla'
-IMAGE_ID = 'e9691262-e286-46f7-aea5-9f40461b5eea'
-NODE_USERNAME = 'ubuntu'
-HADOOP_VERSION = '1.2.1'
-HADOOP_USER = 'hadoop'
-HADOOP_DIRECTORY = '/usr/share/hadoop'
-HADOOP_LOG_DIRECTORY = '/mnt/log/hadoop/hadoop/userlogs'
-HADOOP_PROCESSES_WITH_PORTS = jobtracker: 50030, namenode: 50070, tasktracker: 50060, datanode: 50075, secondarynamenode: 50090
-PROCESS_NAMES = nn: namenode, tt: tasktracker, dn: datanode
-SKIP_ALL_TESTS_FOR_PLUGIN = False
-SKIP_CLUSTER_CONFIG_TEST = False
-SKIP_MAP_REDUCE_TEST = False
-SKIP_SWIFT_TEST = False
-SKIP_SCALING_TEST = False
-[HDP]
-PLUGIN_NAME = 'hdp'
+PATH_TO_SSH = '/home/ubuntu/.ssh/id_rsa'
+ENABLE_HADOOP_TESTS_FOR_HDP_PLUGIN = False
+$Common_parameters
+" >> $WORKSPACE/savanna/tests/integration/configs/common_config.py
+
+echo "PLUGIN_NAME = 'hdp'
 IMAGE_ID = 'cd63f719-006e-4541-a523-1fed7b91fa8c'
 NODE_USERNAME = 'root'
-HADOOP_VERSION = '1.3.0'
-HADOOP_USER = 'hdfs'
-HADOOP_DIRECTORY = '/usr/lib/hadoop'
-HADOOP_LOG_DIRECTORY = '/hadoop/mapred/userlogs'
-HADOOP_PROCESSES_WITH_PORTS = JOBTRACKER: 50030, NAMENODE: 50070, TASKTRACKER: 50060, DATANODE: 50075, SECONDARY_NAMENODE: 50090
-PROCESS_NAMES = nn: NAMENODE, tt: TASKTRACKER, dn: DATANODE
-SKIP_ALL_TESTS_FOR_PLUGIN = False
-SKIP_MAP_REDUCE_TEST = False
-SKIP_SCALING_TEST = False
-" >> $WORKSPACE/savanna/tests/integration_new/configs/itest.conf
+$HDP_parameters
+" >> $WORKSPACE/savanna/tests/integration/configs/hdp_config.py
 
+echo "PLUGIN_NAME = 'vanilla'
+$Vanilla_parameters
+" >> $WORKSPACE/savanna/tests/integration/configs/vanilla_config.py
+
+touch $TMP_LOG
 i=0
 
 while true
 do
         let "i=$i+1"
-        if [ "$i" -gt "240" ]; then
+        diff $TOX_LOG $TMP_LOG
+        cp -f $TOX_LOG $TMP_LOG
+        if [ "$i" -gt "480" ]; then
                 echo "project does not start" && FAILURE=1 && break
         fi
         if [ ! -f $WORKSPACE/log.txt ]; then
@@ -125,6 +104,7 @@ cat /tmp/tox-log.txt
 rm -f /tmp/tox-log.txt
 
 rm -f /tmp/savanna-server.db
+rm $TMP_LOG
 
 if [ "$FAILURE" != 0 ]; then
     exit 1
