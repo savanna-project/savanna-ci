2fcc146f968d83b9ebb199486fd4541c822caa71
Sergey Lukjanov <me@frostman.ru>
2013-11-21 23:08:31 +0300

    Update savanna-check-bps-approver.sh

--- scripts/savanna-check-bps-approver.sh
+++ scripts/savanna-check-bps-approver.sh
@@ -8,13 +8,18 @@ lp = Launchpad.login_anonymously('savanna-bad-bps', 'production',
                                  '/tmp/savanna-bad-bps',
                                  version="devel", timeout=10)
 
-bps = (lp.projects['savanna'].all_specifications 
-        + lp.projects['python-savannaclient'].all_specifications)
+function check_bps_approver(project_name):
+    bps = lp.projects[project_name].all_specifications 
 
-for bp in bps:
-    approver = bp.approver
-    if approver and approver.name == u'slukjanov':
-        continue
-    print "Bad BP: %s (%s)" % (bp.name, bp.web_link)
+    print "Total BPs: %s" % len(bps)
+    for bp in bps:
+        approver = bp.approver
+        if approver and approver.name == u'slukjanov':
+            continue
+        print "Bad BP: %s (%s)" % (bp.name, bp.web_link)
+        
+        
+check_bps_approver("savanna")
+check_bps_approver("python-savannaclient")
 EOL
 python script.py > report.txt
