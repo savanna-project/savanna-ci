736aaed2809d8566562d9565fbb254ba273e4b9b
Sergey Lukjanov <me@frostman.ru>
2013-12-02 18:16:14 +0300

    Update deploy_jobs.sh

--- deploy_jobs.sh
+++ deploy_jobs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find $1 -type f | while read FILENAME; do
-     jenkins-jobs --ignore-cache --conf /etc/jenkins_jobs/jenkins_jobs.ini delete -p "$FILENAME"
+     jenkins-jobs --ignore-cache --conf /etc/jenkins_jobs/jenkins_jobs.ini update "$FILENAME"
 done
