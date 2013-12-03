71c50d93d2c8e2a3b8e01ec1520d6e8c2abf3824
Sergey Kolekonov <skolekonov@mirantis.com>
2013-12-03 17:13:20 +0300

    Update gate-ui-tests.sh

--- scripts/gate-ui-tests.sh
+++ scripts/gate-ui-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-#sudo pip install $WORKSPACE
+sudo pip install $WORKSPACE
 
 SAVANNA_LOG=/tmp/savanna.log 
 
@@ -23,7 +23,7 @@ screen -dmS display sudo Xvfb -fp /usr/share/fonts/X11/misc/ :22 -screen 0 1024x
 
 export DISPLAY=:22
 
-cd /home/ubuntu/savanna && screen -dmS savanna /bin/bash -c "tox -evenv -- savanna-api --config-file etc/savanna/savanna.conf -d --log-file /tmp/savanna.log"
+cd /home/ubuntu && screen -dmS savanna /bin/bash -c "savanna-venv/bin/python savanna-venv/bin/savanna-api --config-file savanna-venv/etc/savanna/savanna.conf -d --log-file /tmp/savanna.log"
 
 while true
 do
@@ -36,12 +36,12 @@ done
 
 echo "
 [common]
-base_url = 'http://127.0.0.1'
+base_url = 'http://127.0.0.1/horizon'
 user = 'ci-user'
 password = 'swordfish'
 tenant = 'ci'
 keystone_url = 'http://172.18.168.2:5000/v2.0'
-await_element = 120
+await_element = 30
 image_name_for_register = 'ci-workers'
 [vanilla]
 skip_plugin_tests = False
@@ -51,45 +51,4 @@ skip_plugin_tests = True
 hadoop_version = '1.3.2'
 " >> $WORKSPACE/savannadashboard/tests/configs/config.conf
 
-cd $WORKSPACE
-if [ -d horizon/ ]
-then
-    rm -rf horizon
-fi
-git clone https://git.openstack.org/openstack/horizon
-cd horizon
-git checkout $BRANCH
-cp openstack_dashboard/local/local_settings.py.example openstack_dashboard/local/local_settings.py
-
-OPENSTACK_HOST=172.18.168.2
-
-sed -i "s/OPENSTACK_HOST = \"127.0.0.1\"/OPENSTACK_HOST = \"$OPENSTACK_HOST\"/g" openstack_dashboard/local/local_settings.py
-sed -i "s/#from horizon.utils import secret_key/from horizon.utils import secret_key/g" openstack_dashboard/local/local_settings.py
-sed -i "s/#OPENSTACK_ENDPOINT_TYPE = \"publicURL\"/OPENSTACK_ENDPOINT_TYPE = \"publicURL\"/g" openstack_dashboard/local/local_settings.py
-sed -i "s/#SECRET_KEY = secret_key.generate_or_read_from_file(os.path.join(LOCAL_PATH, '.secret_key_store'))/SECRET_KEY = secret_key.generate_or_read_from_file(os.path.join(LOCAL_PATH, '.secret_key_store'))/g" openstack_dashboard/local/local_settings.py
-echo -e "SAVANNA_USE_NEUTRON = True" >> openstack_dashboard/local/local_settings.py
-echo -e "AUTO_ASSIGNMENT_ENABLED = False" >> openstack_dashboard/local/local_settings.py
-echo -e "SAVANNA_URL = \"$SAVANNA_URL\"" >> openstack_dashboard/local/local_settings.py
-
-sed -i "s/'openstack_dashboard'/'savannadashboard',\n    'openstack_dashboard'/g" openstack_dashboard/settings.py
-echo "HORIZON_CONFIG['dashboards'] += ('savanna',)" >> openstack_dashboard/settings.py
-
-python tools/install_venv.py
-.venv/bin/pip install ../
-# Horizon doesn't start with Django==1.6 which is installed by default
-.venv/bin/pip uninstall Django -y
-.venv/bin/pip install Django==1.5
-ln -s $WORKSPACE/savanna-dashboard/savannadashboard .venv/lib/python2.7/site-packages/savannadashboard
-
-sudo service apache2 restart
-
 cd $WORKSPACE && tox -e tests
-STATUS=`echo $?`               
-rm -r /tmp/savanna.db   
-screen -S display -X quit
-screen -S savanna -X quit
-
-if [[ "$STATUS" != 0 ]]        
-then                               
-    exit 1                    
-fi 
