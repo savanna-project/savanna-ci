c0cb0da15f314a69b7ebc63c0d027f7740a5d56b
Sergey Kolekonov <skolekonov@mirantis.com>
2013-12-03 15:20:27 +0300

    Update gate-ui-tests.sh

--- scripts/gate-ui-tests.sh
+++ scripts/gate-ui-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo pip install $WORKSPACE
+#sudo pip install $WORKSPACE
 
 SAVANNA_LOG=/tmp/savanna.log 
 
@@ -51,4 +51,45 @@ skip_plugin_tests = True
 hadoop_version = '1.3.2'
 " >> $WORKSPACE/savannadashboard/tests/configs/config.conf
 
+cd $WORKSPACE
+if [ -d horizon/ ]
+then
+    rm -rf horizon
+fi
+git clone https://git.openstack.org/openstack/horizon
+cd horizon
+git checkout $BRANCH
+cp openstack_dashboard/local/local_settings.py.example openstack_dashboard/local/local_settings.py
+
+lab=$(pwd | grep cz)
+if [ -z $lab ]; then
+    OPENSTACK_HOST=172.18.79.139
+else
+    OPENSTACK_HOST=172.18.168.2
+fi
+
+sed -i "s/OPENSTACK_HOST = \"127.0.0.1\"/OPENSTACK_HOST = \"$OPENSTACK_HOST\"/g" openstack_dashboard/local/local_settings.py
+sed -i "s/#from horizon.utils import secret_key/from horizon.utils import secret_key/g" openstack_dashboard/local/local_settings.py
+sed -i "s/#OPENSTACK_ENDPOINT_TYPE = \"publicURL\"/OPENSTACK_ENDPOINT_TYPE = \"publicURL\"/g" openstack_dashboard/local/local_settings.py
+sed -i "s/#SECRET_KEY = secret_key.generate_or_read_from_file(os.path.join(LOCAL_PATH, '.secret_key_store'))/SECRET_KEY = secret_key.generate_or_read_from_file(os.path.join(LOCAL_PATH, '.secret_key_store'))/g" openstack_dashboard/local/local_settings.py
+echo -e "SAVANNA_USE_NEUTRON = True" >> openstack_dashboard/local/local_settings.py
+echo -e "AUTO_ASSIGNMENT_ENABLED = False" >> openstack_dashboard/local/local_settings.py
+echo -e "SAVANNA_URL = \"$SAVANNA_URL\"" >> openstack_dashboard/local/local_settings.py
+
+sed -i "s/'openstack_dashboard'/'savannadashboard',\n    'openstack_dashboard'/g" openstack_dashboard/settings.py
+echo "HORIZON_CONFIG['dashboards'] += ('savanna',)" >> openstack_dashboard/settings.py
+
+python tools/install_venv.py
+.venv/bin/pip install ../
+# Horizon doesn't start with Django==1.6 which is installed by default
+.venv/bin/pip uninstall Django -y
+.venv/bin/pip install Django==1.5
+ln -s $WORKSPACE/savanna-dashboard/savannadashboard .venv/lib/python2.7/site-packages/savannadashboard
+
 cd $WORKSPACE && tox -e tests
+STATUS=`echo $?`               
+rm -r /tmp/savanna.db                              
+if [[ "$STATUS" != 0 ]]        
+then                               
+    exit 1                    
+fi 
