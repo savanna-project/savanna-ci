47b58ec535d9bcba70042211f88c06fc17cb4086
Sergey Kolekonov <skolekonov@mirantis.com>
2013-12-11 11:53:15 +0300

    Update gate-savanna-integration.sh

--- scripts/gate-savanna-integration.sh
+++ scripts/gate-savanna-integration.sh
@@ -68,10 +68,12 @@ $COMMON_PARAMS
 " >> $WORKSPACE/savanna/tests/integration/configs/itest.conf
 
 echo "[VANILLA]
+IMAGE_NAME = 'savanna-itests-ci-vanilla-image' 
 $VANILLA_PARAMS
 " >> $WORKSPACE/savanna/tests/integration/configs/itest.conf
 
 echo "[HDP]
+IMAGE_NAME = 'savanna-itests-ci-hdp-image'
 SKIP_ALL_TESTS_FOR_PLUGIN = False
 $HDP_PARAMS
 " >> $WORKSPACE/savanna/tests/integration/configs/itest.conf
