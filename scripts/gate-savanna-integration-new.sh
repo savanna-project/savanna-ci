8de2eb0950ceb8bf0fa5df1bfad604c3e910e698
Sergey Kolekonov <skolekonov@mirantis.com>
2013-12-22 13:24:23 +0400

    added experimental itests script for the new CI lab

---
+++ scripts/gate-savanna-integration-new.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+export PYTHONUNBUFFERED=1
+
+cd $WORKSPACE
+
+TOX_LOG=$WORKSPACE/.tox/venv/log/venv-1.log 
+TMP_LOG=/tmp/tox.log
+LOG_FILE=/tmp/tox_log.log
+
+screen -S savanna-api -X quit
+rm -f /tmp/savanna-server.db
+rm -rf /tmp/cache
+rm -f $LOG_FILE
+
+BUILD_ID=dontKill
+
+#sudo pip install tox
+mkdir /tmp/cache
+
+export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
+
+echo "[DEFAULT]
+os_auth_host=172.18.168.42
+os_auth_port=5000
+os_admin_username=ci-user
+os_admin_password=nova
+os_admin_tenant_name=ci
+use_neutron=true
+plugins=vanilla,hdp
+[cluster_node]
+[sqlalchemy]
+[plugin:vanilla]
+plugin_class=savanna.plugins.vanilla.plugin:VanillaProvider
+[plugin:hdp]
+plugin_class=savanna.plugins.hdp.ambariplugin:AmbariPlugin" >> etc/savanna/savanna.conf
+
+#touch ~/.pip/pip.conf
+
+#echo "
+#[global] 
+#timeout = 60
+#index-url = http://savanna-ci.vm.mirantis.net/pypi/savanna/
+#extra-index-url = https://pypi.python.org/simple/
+#download-cache = /home/ubuntu/.pip/cache/
+#[install]
+#use-mirrors = true
+#find-links = http://savanna-ci.vm.mirantis.net:8181/simple/
+#" > ~/.pip/pip.conf
+screen -dmS savanna-api /bin/bash -c "PYTHONUNBUFFERED=1 tox -evenv -- savanna-api --config-file etc/savanna/savanna.conf -d --log-file log.txt | tee /tmp/tox-log.txt"
+
+
+export ADDR=`ifconfig eth0| awk -F ' *|:' '/inet addr/{print $4}'`
+
+echo "[COMMON]
+OS_USERNAME = 'ci-user'
+OS_PASSWORD = 'nova'
+OS_TENANT_NAME = 'ci'
+OS_AUTH_URL = 'http://172.18.168.42:5000/v2.0'
+SAVANNA_HOST = '$ADDR'
+FLAVOR_ID = '3'
+CLUSTER_CREATION_TIMEOUT = 60
+CLUSTER_NAME = 'ci-$BUILD_NUMBER-$ZUUL_CHANGE-$ZUUL_PATCHSET'
+FLOATING_IP_POOL = 'public'
+NEUTRON_ENABLED = True
+INTERNAL_NEUTRON_NETWORK = 'private'
+$COMMON_PARAMS
+" >> $WORKSPACE/savanna/tests/integration/configs/itest.conf
+
+echo "[VANILLA]
+IMAGE_NAME = 'savanna-itests-ci-vanilla-image'
+$VANILLA_PARAMS
+" >> $WORKSPACE/savanna/tests/integration/configs/itest.conf
+
+echo "[HDP]
+IMAGE_NAME = 'savanna-itests-ci-hdp-image-jdk'
+SKIP_ALL_TESTS_FOR_PLUGIN = True
+$HDP_PARAMS
+" >> $WORKSPACE/savanna/tests/integration/configs/itest.conf
+
+touch $TMP_LOG
+i=0
+
+while true
+do
+        let "i=$i+1"
+        diff $TOX_LOG $TMP_LOG >> $LOG_FILE
+        cp -f $TOX_LOG $TMP_LOG
+        if [ "$i" -gt "240" ]; then
+                cat $LOG_FILE
+                echo "project does not start" && FAILURE=1 && break
+        fi
+        if [ ! -f $WORKSPACE/log.txt ]; then
+                sleep 10
+        else
+                echo "project is started" && FAILURE=0 && break
+        fi
+done
+
+if [ "$FAILURE" = 0 ]; then
+   
+    export PYTHONUNBUFFERED=1
+   
+    cd $WORKSPACE && tox -e integration
+fi
+
+echo "-----------Python integration env-----------"
+cd $WORKSPACE && .tox/integration/bin/pip freeze
+
+screen -S savanna-api -X quit
+
+echo "-----------Python savanna env-----------"
+cd $WORKSPACE && .tox/venv/bin/pip freeze
+
+echo "-----------Savanna Log------------"
+cat $WORKSPACE/log.txt
+rm -rf /tmp/workspace/
+rm -rf /tmp/cache/
+
+echo "-----------Tox log-----------"
+cat /tmp/tox-log.txt
+rm -f /tmp/tox-log.txt
+
+rm -f /tmp/savanna-server.db
+rm $TMP_LOG
+rm -f $LOG_FILE
+
+if [ "$FAILURE" != 0 ]; then
+    exit 1
+fi
