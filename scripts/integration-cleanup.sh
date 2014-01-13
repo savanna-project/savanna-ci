53aa3a931d64b56830bc3f24e2874a6da964477e
Sergey Kolekonov <skolekonov@mirantis.com>
2014-01-13 16:36:31 +0400

    added integration-cleanup job

---
+++ scripts/integration-cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+JOB_TYPE=$(echo $JOB_NAME | awk -F '-' '{ print $4 }')                          
+                                                                                                                                                                
+if [ $JOB_TYPE == 'heat' ]                                                      
+then                                                                                                                                   
+    JOB_TYPE=$(echo $JOB_NAME | awk -F '-' '{ print $5 }')                      
+    if [ $JOB_TYPE == 'hdp'  ]                                                  
+    then                                                                        
+        python /var/lib/jenkins/ci-python-scripts/prepare_vm.py cleanup-heat ci-$PREV_BUILD-hdp 
+    else                                                                        
+        python /var/lib/jenkins/ci-python-scripts/prepare_vm.py cleanup-heat ci-$PREV_BUILD-vanilla                                              
+    fi                                                                          
+else                                                                            
+    python /var/lib/jenkins/ci-python-scripts/prepare_vm.py cleanup $PREV_BUILD                            
+fi
