b19e7565525aadd6cd5f2d5bb272ad0b2cb4047e
Sergey Kolekonov <skolekonov@mirantis.com>
2014-01-29 18:25:42 +0300

    Update gate-savanna-integration-new.sh

--- scripts/gate-savanna-integration-new.sh
+++ scripts/gate-savanna-integration-new.sh
@@ -9,7 +9,7 @@ JOB_TYPE=$(echo $JOB_NAME | awk -F '-' '{ print $4 }')
 if [ $JOB_TYPE == 'heat' ]                                                      
 then                                                                            
     HEAT_JOB=True
-    HDP_IMAGE=centos-6_4-64-hdp-1.3-neutron-updated
+    HDP_IMAGE=centos-6_4-64-hdp-1.3-neutron
     VANILLA_IMAGE=savanna-itests-ci-vanilla-image-heat
     SSH_USERNAME=ec2-user
     echo "Heat detected"
@@ -26,7 +26,7 @@ else
     if [ $JOB_TYPE == 'hdp' ]                                                   
     then                                                                        
        HDP_JOB=True
-       HDP_IMAGE=centos-6_4-64-hdp-1.3-neutron-updated
+       HDP_IMAGE=centos-6_4-64-hdp-1.3-neutron
        echo "HDP detected"
     else                                                                        
        VANILLA_JOB=True 
