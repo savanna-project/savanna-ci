f621e686b1ca339326e44a0b6906d1dd2ea5fb41
Sergey Kolekonov <skolekonov@mirantis.com>
2014-02-10 11:29:03 +0300

    Update update_pool.sh

--- scripts/update_pool.sh
+++ scripts/update_pool.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source /opt/ci/nodepool/bin/activate
-
-for i in $(nodepool list | grep ci-lab | awk -F '|' '{ print $2 }'); do nodepool delete $i; done
+jenkins-cli -s http://127.0.0.1:8080/jenkins quiet-down
+sleep 5
+for i in $(nodepool-client list | grep ci-lab | awk -F '|' '{ print $2 }'); do nodepool-client delete $i; done
+jenkins-cli -s http://127.0.0.1:8080/jenkins cancel-quiet-down
