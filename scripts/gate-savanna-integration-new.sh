4d5388426473facb0d73e7f97e287e39d4b8bdab
Sergey Kolekonov <skolekonov@mirantis.com>
2014-02-21 15:45:43 +0300

    Update gate-savanna-integration-new.sh

--- scripts/gate-savanna-integration-new.sh
+++ scripts/gate-savanna-integration-new.sh
@@ -150,7 +150,7 @@ $HDP_PARAMS
 " >> $WORKSPACE/savanna/tests/integration/configs/itest.conf
 
 echo "[IDH]
-IMAGE_ID = 'c0492fb9-a6b6-4d78-867b-b5be12fe6611'
+IMAGE_NAME = '$IDH_IMAGE'
 IDH_REPO_URL = 'http://172.18.87.221/mirror/inteldistribution-2.5.1'
 OS_REPO_URL = 'http://172.18.87.221/mirror/centos/base/'
 SSH_USERNAME = 'cloud-user'
