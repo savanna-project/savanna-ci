de105953b6f1db267fd97bb4385a0e163cb217ca
Sergey Kolekonov <skolekonov@mirantis.com>
2014-03-28 22:08:31 +0300

    Update integration-cleanup.sh

--- scripts/integration-cleanup.sh
+++ scripts/integration-cleanup.sh
@@ -4,7 +4,11 @@ JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $4 }')
                                                                                                                                                                 
 if [ $JOB_TYPE == 'heat' ]                                                      
 then                                                                                                                                   
-    JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $5 }')                      
+    JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $5 }')
+    if [ $JOB_TYPE == 'vanilla1' ]
+    then
+        JOB_TYPE=vanilla-v1
+    fi
     python /var/lib/jenkins/ci-python-scripts/prepare_vm.py cleanup-heat ci-$PREV_BUILD-$JOB_TYPE
 else                                                                            
     python /var/lib/jenkins/ci-python-scripts/prepare_vm.py cleanup $PREV_BUILD                            
