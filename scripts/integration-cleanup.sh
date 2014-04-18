32d33119b4019001205e3dec51af48f723fc5e93
Sergey Kolekonov <skolekonov@mirantis.com>
2014-04-18 15:07:51 +0400

    Update integration-cleanup.sh

--- scripts/integration-cleanup.sh
+++ scripts/integration-cleanup.sh
@@ -3,7 +3,14 @@
 sleep 20
 cd /opt/ci/jenkins-jobs/savanna-ci/scripts
 JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $4 }')                          
-                                                                                                                                                                
+if [ $JOB_TYPE == 'vanilla1' ]
+then
+     JOB_TYPE=vanilla-v1
+elif [ $JOB_TYPE == 'vanilla2' ]
+then
+     JOB_TYPE=vanilla-v2
+fi
+
 if [ $JOB_TYPE == 'heat' ]                                                      
 then                                                                                                                                   
     JOB_TYPE=$(echo $PREV_JOB | awk -F '-' '{ print $5 }')
@@ -13,5 +20,5 @@ then
     fi
     python cleanup.py cleanup-heat ci-$PREV_BUILD-$JOB_TYPE
 else                                                                            
-    python cleanup.py cleanup -$PREV_BUILD-
+    python cleanup.py cleanup -$PREV_BUILD-$JOB_TYPE
 fi
